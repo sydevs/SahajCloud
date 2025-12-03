59ac79e428b6752b3c856aea3941753e82ce73e7
Devindra <devindra.payment@gmail.com>
2025-12-03 13:11:55 +0000

    Add migration

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20251127_204442 from './20251127_204442';
 import * as migration_20251202_175640 from './20251202_175640';
+import * as migration_20251203_131114 from './20251203_131114';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20251202_175640.up,
     down: migration_20251202_175640.down,
-    name: '20251202_175640'
+    name: '20251202_175640',
+  },
+  {
+    up: migration_20251203_131114.up,
+    down: migration_20251203_131114.down,
+    name: '20251203_131114'
   },
 ];
