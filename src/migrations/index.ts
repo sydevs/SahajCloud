d7fa1af6939e0dcb46ab66a3439061ef33e612cf
Devindra <devindra.payment@gmail.com>
2026-01-14 21:16:09 +0800

    Create new schema

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,1 +1,9 @@
-export const migrations = []
+import * as migration_20260114_131548 from './20260114_131548';
+
+export const migrations = [
+  {
+    up: migration_20260114_131548.up,
+    down: migration_20260114_131548.down,
+    name: '20260114_131548'
+  },
+];
