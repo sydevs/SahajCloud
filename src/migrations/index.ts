a5deb84b0b215da86b15dcf0431b9d802454de20
Devindra <devindra.payment@gmail.com>
2026-01-14 21:15:20 +0800

    Reset migrations again

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,1 @@
-import * as migration_20260114_092103 from './20260114_092103'
-
-export const migrations = [
-  {
-    up: migration_20260114_092103.up,
-    down: migration_20260114_092103.down,
-    name: '20260114_092103',
-  },
-]
+export const migrations = []
