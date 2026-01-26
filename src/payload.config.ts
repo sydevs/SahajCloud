221e997c3d9d20d10479f4df1ec8854c56302f35
Devindra <devindra.payment@gmail.com>
2026-01-26 19:40:04 +0800

    Add explicit cors/csrf

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -70,8 +70,18 @@ const payloadConfig = (overrides?: Partial<Config>) => {
       defaultLocale: DEFAULT_LOCALE,
       filterAvailableLocales,
     },
-    cors: [serverUrl, serverEnv.WEMEDITATE_WEB_URL, serverEnv.SAHAJATLAS_URL],
-    csrf: [serverUrl, serverEnv.WEMEDITATE_WEB_URL, serverEnv.SAHAJATLAS_URL],
+    cors: [
+      'https://wemeditate-web.contact-c66.workers.dev',
+      serverUrl,
+      serverEnv.WEMEDITATE_WEB_URL,
+      serverEnv.SAHAJATLAS_URL,
+    ],
+    csrf: [
+      'https://wemeditate-web.contact-c66.workers.dev',
+      serverUrl,
+      serverEnv.WEMEDITATE_WEB_URL,
+      serverEnv.SAHAJATLAS_URL,
+    ],
     admin: {
       user: Managers.slug,
       importMap: {
