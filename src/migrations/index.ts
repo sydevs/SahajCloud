e1462a50af4521efeaefb80b1230dbdb5d6d2118
Devindra <devindra.payment@gmail.com>
2026-01-26 12:44:00 +0800

    Migration for lexical blocks

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260122_065154 from './20260122_065154';
 import * as migration_20260125_051733 from './20260125_051733';
+import * as migration_20260126_104313 from './20260126_104313';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260125_051733.up,
     down: migration_20260125_051733.down,
-    name: '20260125_051733'
+    name: '20260125_051733',
+  },
+  {
+    up: migration_20260126_104313.up,
+    down: migration_20260126_104313.down,
+    name: '20260126_104313',
   },
 ];
