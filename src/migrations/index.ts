60c13090e22cc073c7834b597f389ec6dd1f40c4
Devindra <devindra.payment@gmail.com>
2026-01-28 21:07:55 +0800

    Add missing migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20260122_065154 from './20260122_065154';
 import * as migration_20260125_051733 from './20260125_051733';
 import * as migration_20260126_104313 from './20260126_104313';
+import * as migration_20260128_130731 from './20260128_130731';
 
 export const migrations = [
   {
@@ -17,5 +18,10 @@ export const migrations = [
     up: migration_20260126_104313.up,
     down: migration_20260126_104313.down,
     name: '20260126_104313',
+  },
+  {
+    up: migration_20260128_130731.up,
+    down: migration_20260128_130731.down,
+    name: '20260128_130731'
   },
 ];
